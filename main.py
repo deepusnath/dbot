1fb382ea25174c8848ce1fda93bd7af08776348f
Deepu S Nath <deepusnath@gmail.com>
2021-07-04 03:43:48 +0000

    adds events

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN')) 
